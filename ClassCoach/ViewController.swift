5fabaeee503db7a1f2210343c029e54dafe4eb5e
Samuel Hobel <samuel.hobel@loni.usc.edu>
2017-09-05 21:48:10 -0700

    Initial Commit

---
+++ ClassCoach/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClassCoach
+//
+//  Created by Samuel Hobel on 9/5/17.
+//  Copyright © 2017 Samuel Hobel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
