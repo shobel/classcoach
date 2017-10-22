1a5f488f94d274020b794784d9f4a14a9f705c66
Samuel Hobel <samuel.hobel@loni.usc.edu>
2017-10-22 13:40:59 -0700

    encryption

--- ClassCoach/DataHolder.swift
+++ ClassCoach/DataHolder.swift
@@ -26,7 +26,7 @@ class DataHolder {
         //3 - creates a new path component and creates a new file called "Data" which is where we will store our Data array.
         return (url!.appendingPathComponent("Data").path)
     }
-    let keycode = "password"
+
     static let sharedInstance = DataHolder()
     public var classList : [Student]! {
         didSet {
@@ -54,7 +54,7 @@ class DataHolder {
     public func saveData(){
         //NSKeyedArchiver.archiveRootObject(classList, toFile: filePath)
         let archivedData = NSKeyedArchiver.archivedData(withRootObject: classList)
-        let encryptedData = RNCryptor.encrypt(data: archivedData, withPassword: keycode)
+        let encryptedData = RNCryptor.encrypt(data: archivedData, withPassword: Crypt.key)
         NSKeyedArchiver.archiveRootObject(encryptedData, toFile: filePath)
     }
     
@@ -69,7 +69,7 @@ class DataHolder {
         } else {
             if let encryptedArchive = NSKeyedUnarchiver.unarchiveObject(withFile: filePath) as? Data {
                 do {
-                    let decryptedArchive = try RNCryptor.decrypt(data: encryptedArchive, withPassword: keycode)
+                    let decryptedArchive = try RNCryptor.decrypt(data: encryptedArchive, withPassword: Crypt.key)
                     let classData = NSKeyedUnarchiver.unarchiveObject(with: decryptedArchive) as! [Student]
                     if (validate(classList: classData)){
                         classList = classData
---
+++ ClassCoach/Private.swift
@@ -0,0 +1,9 @@
+//
+//  Private.swift
+//  ClassCoach
+//
+//  Created by Samuel Hobel on 10/22/17.
+//  Copyright © 2017 Samuel Hobel. All rights reserved.
+//
+
+import Foundation
